24478759b701e780801ca4b44e9dac74eed066e6
macfa <kombo6768@gmail.com>
2024-01-28 17:52:47 +0900

    D-1 for build a portfolio site

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#typed", {
+  strings: ['Designer', 'Developer', 'Freelancer', 'Photographer'],
+  typeSpeed: 90,
+  backSpeed: 70,
+  backDelay: 100,
+  startDelay: 100,
+  loop: true,
+});
