3e2484e1b49cfd1ce9c23d2a1e3e63eb05e83cb6
macfa <kombo6768@gmail.com>
2024-03-02 05:43:53 +0900

    Delete dummy folders

--- nextjs-dashboard/app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Inter, Istok_Web } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'] });
-
-export const lusitana = Istok_Web({ subsets: ['cyrillic'], weight: ["400"], style: "italic" });
